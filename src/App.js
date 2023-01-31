9176b1ef2d7fcda654bdffe2ac3ab8b21e8b7c0b
Tyler <tylerfarhner@gmail.com>
2023-01-31 01:08:01 -0800

    Fixes imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import logo from './logo.svg';
 import './App.css';
-import { createBrowserRouter, createRoutesFromElements, Route, Link, Outlet } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, Link, Outlet, RouterProvider } from 'react-router-dom'
 
-import { Home } from './routes/Home.jsx'
-import { Data } from './routes/Data.jsx'
-import { Contact } from './routes/Contact.jsx'
+import Home from './routes/Home'
+import Data from './routes/Data';
+import Contact from './routes/Contact';
 
 function App() {
 
@@ -26,18 +26,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <RouterProvider router={router}/>
       </header>
     </div>
   );
